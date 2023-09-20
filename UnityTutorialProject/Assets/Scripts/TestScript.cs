81c9a3a4a8009b67ae0fdc368278196e137d4820
DaneSlack <danerslack06@gmail.com>
2023-09-20 13:01:15 +0100

    Added Duck, Moves Duck and Added Text

--- UnityTutorialProject/Assets/Scripts/TestScript.cs
+++ UnityTutorialProject/Assets/Scripts/TestScript.cs
@@ -7,12 +7,18 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("What Do You Want!");
+        Debug.Log("You're Still Here!");
+        Debug.Log("Go Away!");
+
+     
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Words");
         
+        transform.Translate(0.02f, 0, 0);
     }
 }
